409aba5a1734d8908c7a7546f87e59cf206e9d56
INao-mp <prividenbko@gmail.com>
2021-07-22 16:21:38 +0300

    D2
    D2 start : Re-movement, Re-model, Anim

---
+++ PL1/Assets/PlayerMovement.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public CharacterController2D controller;
+    public Animator animator;
+
+    public float runSpeed = 40f;
+    float HorMove = 0f;
+    bool jump = false;
+    bool crouch = false;
+
+    void Update()
+    {
+        HorMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+
+        animator.SetFloat("Speed", Mathf.Abs(HorMove));
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            jump = true;
+            animator.SetBool("IsJump", true);
+        }
+
+        if (Input.GetButtonDown("Crouch"))
+        {
+            crouch = true;
+        }else if (Input.GetButtonUp("Crouch"))
+        {
+            crouch = false;
+        }
+    }
+
+    public void OnLanding()
+    {
+        animator.SetBool("IsJump", false);
+    }
+
+    void FixedUpdate()
+    {
+        controller.Move(HorMove * Time.deltaTime, crouch, jump);
+        jump = false;
+    }
+}
