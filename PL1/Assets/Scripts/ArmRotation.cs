9b2e9e83603bbf36db0375519330a8b8f65b6c68
INao-mp <prividenbko@gmail.com>
2021-07-23 19:23:32 +0300

    D3 red.
    Bullet trials, code change, slash.

---
+++ PL1/Assets/Scripts/ArmRotation.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class ArmRotation : MonoBehaviour
+{
+    public int rotOffset = 0;
+
+    void Update()
+    {
+        Vector3 dif = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+        dif.Normalize();
+
+        float rotZ = Mathf.Atan2(dif.y, dif.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(0f, 0f, rotZ + rotOffset);
+    }
+}
---
+++ PL1/Assets/Scripts/MoveTrial.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class MoveTrial : MonoBehaviour
+{
+    public int MoveSpeed = 20;
+
+    void Update()
+    {
+        transform.Translate(Vector3.right * Time.deltaTime * MoveSpeed);
+        Destroy(gameObject, 1);
+
+    }
+}
