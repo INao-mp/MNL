c5316594b449414785c19563e16a41743f56768b
INao-mp <prividenbko@gmail.com>
2021-07-23 21:36:30 +0300

    D3 end.
    Respawn, health, REffect

---
+++ PL1/Assets/GM_Main.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class GM_Main : MonoBehaviour
+{
+    public static GM_Main gm;
+
+    void Start()
+    {
+        if (gm == null)
+        {
+            gm = this;
+        }    
+    }
+
+    public Transform playerPrefabs;
+    public Transform spawnPoint;
+    public int spawnDelay = 2;
+    public Transform spawnPrefabs;
+    public AudioSource _audio;
+    
+    public IEnumerator RespawnPlayer()
+    {
+        _audio.Play();
+        yield return new WaitForSeconds(spawnDelay);
+
+        Instantiate(playerPrefabs, spawnPoint.position, spawnPoint.rotation);
+        Transform clone = Instantiate(spawnPrefabs, spawnPoint.position, spawnPoint.rotation);
+        Destroy(clone.gameObject, 3f);
+    }
+
+    public static void KillPlayer(Player player)
+    {
+        Destroy(player.gameObject);
+        gm.StartCoroutine(gm.RespawnPlayer());
+    }
+}
---
+++ PL1/Assets/Player.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+public class Player : MonoBehaviour
+{   [System.Serializable]
+    public class PlayerStats
+    {
+        public int health = 100;
+    }
+
+    public PlayerStats playerStats = new PlayerStats();
+
+    public int fallPos = -20;
+
+    private void Update()
+    {
+        if (transform.position.y<= fallPos)
+        {
+            DamagePlayer(99999);
+        }
+    }
+
+    public void DamagePlayer(int damage)
+    {
+        playerStats.health -= damage;
+
+        if (playerStats.health <= 0)
+        {
+            GM_Main.KillPlayer(this);
+            Debug.Log("You Die!");
+        }
+    }
+
+}
