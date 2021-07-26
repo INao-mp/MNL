5b4e1eab69b8c33df92ab8cb1d620c0dcf616660
INao-mp <prividenbko@gmail.com>
2021-07-26 18:51:00 +0300

    D5
    A* pathfinder + AI path

--- PL1/Assets/Enemy_AI.cs
+++ PL1/Assets/Enemy_AI.cs
@@ -1,17 +1,100 @@
 ﻿using UnityEngine;
+using System.Collections;
 using Pathfinding;
 
+[RequireComponent(typeof (Rigidbody2D))]
+[RequireComponent(typeof(Seeker))]
 public class Enemy_AI : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public Transform target;            // what chase
+    public Path path;                   // calculate path
+    public ForceMode2D fMode;
+    public float updateReate = 2f;
+    public float speed = 300f;
+    public float nextWayDis = 3;
+
+    [HideInInspector]
+    public bool pathIsEnd = false;
+
+    private Seeker seeker;
+    private Rigidbody2D rb;
+    private int currentWayPoint = 0;
+
     void Start()
     {
-        
+        seeker = GetComponent<Seeker>();
+        rb = GetComponent<Rigidbody2D>();
+
+        if (target == null)
+        {
+            Debug.LogError("No target");
+            return;
+        }
+
+        seeker.StartPath (transform.position, target.position, OnPathComplete);
+        StartCoroutine (UpdatePath());
     }
 
-    // Update is called once per frame
-    void Update()
+    IEnumerator UpdatePath()
     {
-        
+        if (target == null)
+        {
+             yield return false;
+        }
+
+        seeker.StartPath (transform.position, target.position, OnPathComplete);
+
+        yield return new WaitForSeconds(1f / updateReate);
+        StartCoroutine (UpdatePath());
+    }
+
+    public void OnPathComplete(Path p)
+    {
+        Debug.Log("Error -> check = " + p.error);
+        if (!p.error)
+        {
+            path = p;
+            currentWayPoint = 0;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        if (target == null)
+        {
+            return;
+        }
+
+        if (path == null)
+        {
+            return;
+        }
+
+        if (currentWayPoint >= path.vectorPath.Count)
+        {
+            if (pathIsEnd)
+            {
+                return;
+            }
+
+            Debug.Log("End!");
+            pathIsEnd = true;
+            return;
+        }
+
+        pathIsEnd = false;
+
+        Vector3 dir = (path.vectorPath[currentWayPoint] - transform.position).normalized;
+        dir *= speed * Time.fixedDeltaTime;
+
+        rb.AddForce(dir, fMode);
+
+        float dist = Vector3.Distance(transform.position, path.vectorPath[currentWayPoint]);
+
+        if (dist < nextWayDis)
+        {
+            currentWayPoint++;
+            return;
+        }
     }
 }
