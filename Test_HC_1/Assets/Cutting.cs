f52637bad77121ad5d87ea4ded57106b09597d17
INao-mp <prividenbko@gmail.com>
2021-08-18 21:18:23 +0300

    New 3

---
+++ Test_HC_1/Assets/Cutting.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+public class Cutting : MonoBehaviour
+{
+    private int hitTime = 0;
+    private float cutTime = 0;
+    [SerializeField] private Transform particles;
+
+
+    private void Update()
+    {
+        cutTime -=3/Time.deltaTime;
+
+    }
+
+    private void OnTriggerEnter(Collider collision)
+    {
+        if (collision.gameObject.CompareTag("Knife") && cutTime <=0)
+        {
+            transform.localScale -= new Vector3(0.1f, 0.1f, 0.1f);
+            hitTime++;
+            // Debug.Log("EEEEEEE!" + hitTime);
+            Transform cutEff = (Transform)Instantiate(particles, transform.position, transform.rotation);
+            Destroy(cutEff.gameObject, 2f);
+            cutTime = 1f;
+
+            if (hitTime == 3)
+            {
+
+                Destroy(this.gameObject);
+            }
+        }
+
+    }
+
+}
