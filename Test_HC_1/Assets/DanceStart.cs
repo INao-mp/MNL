953e8fed285aa9c1a06957b0faebf1a1f0a90cbc
INao-mp <prividenbko@gmail.com>
2021-08-21 18:23:40 +0300

    New One
    change idea.  Work on cutting script. Replace and rework main obj

---
+++ Test_HC_1/Assets/DanceStart.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class DanceStart : MonoBehaviour
+{
+    private Animator animator;
+    public int timer = 3;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        if (timer <= 0 )
+        {
+            animator.enabled = true;
+        }   
+    }
+}
---
+++ Test_HC_1/Assets/Scripts/Cutting.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+
+public class Cutting : MonoBehaviour
+{
+    [SerializeField] private DanceStart stick;
+    [SerializeField] private Transform particleSpawnPoint;
+    [SerializeField] private Transform particles;
+    [SerializeField] private float deathTimer = 0;
+    [SerializeField] private bool scale = false;
+
+    private int hitTime = 0;
+    private float cutTime = 0;
+
+    private void Update()
+    {
+        cutTime -= 3/Time.deltaTime;
+    }
+
+    private void OnTriggerEnter(Collider collision)
+    {
+        if (collision.gameObject.CompareTag("Knife") && cutTime <= 0)
+        {
+            if (scale == true)
+            {
+                transform.localScale -= new Vector3(0.1f, 0.1f, 0.1f);
+            }
+
+            Transform cutEff = (Transform)Instantiate(particles, particleSpawnPoint.position, particleSpawnPoint.rotation);
+            Destroy(cutEff.gameObject, 2f);
+            hitTime++;
+            cutTime = 1f;
+
+            if (hitTime == deathTimer)
+            {
+                Destroy(this.gameObject);
+                if (this.gameObject.CompareTag("BasePlate"))
+                {
+                    stick.timer--;
+                }
+            }
+        }
+    }
+}
